bd68cd795b649365d55fa95583cb5bf059aabded
shebnDev <dev@shebn.io>
2025-08-02 11:29:19 -0500

    ajuste import signup

--- page/api/auth/signup.js
+++ page/api/auth/signup.js
@@ -1,4 +1,4 @@
-import { supabase } from '../../../src/app/SupabaseClient';
+import { createClient } from '@supabase/supabase-js';
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
