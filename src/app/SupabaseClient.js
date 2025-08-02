63eaaf29b6f2bb9bef96d476510bc0d4ac7ad437
lgtapi <tu-email@ejemplo.com>
2025-08-01 20:07:24 -0500

    Ajustes variasbles entroni

--- src/app/SupabaseClient.js
+++ src/app/SupabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = 'https://fcvhshxkatnrzrsvbnpo.supabase.co';
-const supabaseAnonKey  = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZjdmhzaHhrYXRucnpyc3ZibnBvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI1MjM5ODMsImV4cCI6MjA2ODA5OTk4M30.rgC8VIle34eyCWZkEIVGREFeebRJYxtJhBDfmxlIJVY';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
